2d8f3f6d307b1e508d326ee85f0d39e74890fb91
Davi Augusto Boff <daviboff783@gmail.com>
2023-04-03 22:04:26 -0300

    ex8

--- lista1/src/lista1/CriaBanco.java
+++ lista1/src/lista1/CriaBanco.java
@@ -9,35 +9,52 @@ import java.sql.Statement;
 
 public class CriaBanco {
 
-	 public static void main(String[] args) {
 
-	        // Definindo as credenciais de acesso ao banco de dados
-	        String url = "jdbc:mysql://localhost:3306/"; // URL do banco de dados
-	        String user = "root"; // Nome do usuário
-	        String password = "senha"; // Senha do usuário
-
-	        // Conectando ao banco de dados
-	        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-
-	            // Criando um novo banco de dados
-	            String nomeBanco = "meu_banco";
-	            String sqlCreateDatabase = "CREATE DATABASE IF NOT EXISTS " + nomeBanco;
-	            try (Statement stmt = conn.createStatement()) {
-	                stmt.execute(sqlCreateDatabase);
-	            }
-
-	            // Criando uma tabela com três colunas, incluindo a chave primária
-	            String nomeTabela = "minha_tabela";
-	            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS " + nomeBanco + "." + nomeTabela +
-	                    " (id INT PRIMARY KEY AUTO_INCREMENT, coluna1 VARCHAR(50), coluna2 VARCHAR(50))";
-	            try (Statement stmt = conn.createStatement()) {
-	                stmt.execute(sqlCreateTable);
-	            }
-
-	            System.out.println("Banco de dados e tabela criados com sucesso!");
-
-	        } catch (SQLException ex) {
-	            System.out.println("Erro ao conectar ou criar banco de dados: " + ex.getMessage());
-	        }
-	    }
+    public static void main(String[] args) {
+        
+        Connection conn = null;
+        Statement stmt = null;
+        
+        try {
+            // Faz a conexão com o banco de dados
+            String url = "jdbc:mysql://localhost/";
+            String user = "root";
+            String password = "aluno";
+            conn = DriverManager.getConnection(url, user, password);
+            
+            // Cria o banco de dados
+            stmt = conn.createStatement();
+            String sql = "CREATE DATABASE meubanco";
+            stmt.executeUpdate(sql);
+            
+            // Seleciona o banco de dados criado
+            sql = "USE meubanco";
+            stmt.executeUpdate(sql);
+            
+            // Cria a tabela
+            sql = "CREATE TABLE minhaTabela " +
+                    "(id INT NOT NULL AUTO_INCREMENT, " +
+                    " coluna1 VARCHAR(255), " +
+                    " coluna2 VARCHAR(255), " +
+                    " PRIMARY KEY (id))";
+            stmt.executeUpdate(sql);
+            
+            System.out.println("Banco de dados e tabela criados com sucesso!");
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro ao criar banco de dados ou tabela: " + ex.getMessage());
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Erro ao fechar conexão com o banco de dados: " + ex.getMessage());
+            }
+        }
+        
+    }
 }
